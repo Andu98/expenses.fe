a0a2fb45c95d697f8fbd8302737c0fb7551fa1ba
Andu98 <tudor_alexandru98@yahoo.com>
2024-07-09 20:43:54 +0300

    First commit

---
+++ src/services/expenses.js
@@ -0,0 +1,12 @@
+export const GetExpenses = async (dispatch) =>{
+    try{
+        //fake api call fow now
+        const expenses = [
+            { id:1, description: "Cumparaturi", amount: 23.20},
+            { id:2, description: "Carburant", amount: 50.00},
+            { id:3, description: "Restaurant", amount: 150.00},
+        ]
+    }catch{
+        console.log("Error!")
+    }
+}

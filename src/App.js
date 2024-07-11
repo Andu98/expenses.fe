d1c7fce22fd2fbe1823b63db2a5fd1f459d7454c
Andu98 <tudor_alexandru98@yahoo.com>
2024-07-11 09:48:18 +0300

    Added ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
 
 const App = () => {
   return(
-   <div style={{width: '60%', margin:'auto'}}>
-   <h3> Your Expenses</h3>
+   <div style={{width: '60%', margin:'auto', marginTop:'40px'}}>
+      <h3 style={{ marginBottom:'40px'}}> New Expenses</h3>
+      <ExpenseForm/>
+   <h3 style={{ marginBottom:'40px'}}> Your Expenses</h3>
    <ExpenseList/>
    </div>
   );
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,32 @@
 import {React,useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch,useSelector} from 'react-redux';
 import { GetExpenses } from '../services/expenses';
-
+import { Row, Col, Button } from 'react-bootstrap';
 export default () => {
     const dispatch = useDispatch();
-
     useEffect(()=>{
         GetExpenses(dispatch);
     },[]);
+
+    //this extracts data from the redux store state (din store.js)
+    const expenses = useSelector(state => state.expensesReducer.expenses);
+    
+    return expenses.map((expense) => {
+        return(
+            <div style={{ marginBottom: '1rem' }} key={expense.id}>
+               <ListRow expense={expense}/>
+            </div>
+        );
+    });
+}
+
+const ListRow = ({expense}) => {
+    return <div>
+        <Row>
+            <Col>{expense.description}</Col>
+            <Col>{expense.amount}</Col>
+           <Col><Button variant="warning">Edit</Button></Col> 
+        </Row>
+        <hr />
+    </div>
 }
